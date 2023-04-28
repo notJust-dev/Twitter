16e030c915c4c2cdafa0d194eb77312e94c0ff78
Vadim Savin <savinvadim1312@gmail.com>
2023-04-28 15:21:31 +0100

    init

--- app/modal.tsx
+++ app/modal.tsx
@@ -7,8 +7,12 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Hello world</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
---
+++ assets/data/tweets.ts
@@ -0,0 +1,137 @@
+const tweets = [
+  {
+    id: 't0',
+    user: {
+      id: 'u1',
+      username: 'VadimNotJustDev',
+      name: 'Vadim',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content: 'Can you please check if the Subscribe button on Youtube works?',
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/thumbnail.png',
+    numberOfComments: 123,
+    numberOfRetweets: 11,
+    numberOfLikes: 10,
+  },
+  {
+    id: '111111111',
+    createdAt: '2023-04-28T08:30:00.000Z',
+    user: {
+      id: '123456789',
+      name: 'Jeff B',
+      username: 'bezos',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+    },
+    content:
+      'Just had a great workout at the gym! 💪 #fitness #healthylifestyle',
+    numberOfComments: 2,
+    numberOfRetweets: 5,
+    numberOfLikes: 25,
+    impressions: 500,
+  },
+  {
+    id: '222222222',
+    createdAt: '2023-04-27T19:45:00.000Z',
+    user: {
+      id: '987654321',
+      name: 'Zuck',
+      username: 'zuck',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
+    },
+    content: 'Had an amazing surf session this morning',
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/8.jpg',
+    numberOfComments: 10,
+    numberOfRetweets: 20,
+    numberOfLikes: 100,
+    impressions: 1000,
+  },
+  {
+    id: '333333333',
+    createdAt: '2023-04-26T12:00:00.000Z',
+    user: {
+      id: '123456789',
+      name: 'Jane Smith',
+      username: 'janesmith',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/6.png',
+    },
+    content:
+      'Excited to announce that I will be speaking at the upcoming tech conference in San Francisco! 🎉 #womenintech',
+    numberOfComments: 5,
+    numberOfRetweets: 10,
+    numberOfLikes: 50,
+    impressions: 1000,
+  },
+  {
+    id: 't1',
+    user: {
+      id: 'u1',
+      username: 'VadimNotJustDev',
+      name: 'Vadim',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image:
+      'https://i.insider.com/5d03aa8e6fc9201bc7002b43?width=1136&format=jpeg',
+    numberOfComments: 123,
+    numberOfRetweets: 11,
+    numberOfLikes: 10,
+  },
+  {
+    id: 't2',
+    user: {
+      id: 'u1',
+      username: 'VadimNotJustDev',
+      name: 'Vadim',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Hey Hey Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+  {
+    id: 't3',
+    user: {
+      id: 'u1',
+      username: 'VadimNotJustDev',
+      name: 'Vadim',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content: 'Hello World',
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+  {
+    id: 't4',
+    user: {
+      id: 'u1',
+      username: 'VadimNotJustDev',
+      name: 'Vadim',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Hey Hey Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+];
+
+export default tweets;
